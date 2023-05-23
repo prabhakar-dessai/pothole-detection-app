2f3256279d409d6bb4cc0bb5797d02c1f360b967
MograsysInterns <61088215+prabhakar-dessai@users.noreply.github.com>
2023-05-23 13:37:31 +0530

    update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,request,render_template
 from werkzeug.utils import secure_filename
 import os
+from main import process_file
 
 app = Flask(__name__)
 
 app.config['ALLOWED_EXTENSIONS'] = {'mp4'}
-app.config['UPLOAD_FOLDER'] = 'static/videos/'
+app.config['UPLOAD_FOLDER'] = 'static/videos/input'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -19,10 +20,10 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['video']
         if file and allowed_file(file.filename):
-            # filename = secure_filename(file.filename) #not working rn
+            filename = secure_filename(file.filename) 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
-        #     processed_filename = process_file(filename)
-            return render_template('success.html', filename=file.filename) # change after including processing step
+            # processed_filename = process_file(filename) # code in main.py
+            return render_template('success.html', filename=filename) # change after including processing step
     return render_template('index.html')
 
 
---
+++ main.py
@@ -0,0 +1,4 @@
+def process_file(filename):
+    # model running on filename to give final product 
+    # save product in output
+    return processed_filename
